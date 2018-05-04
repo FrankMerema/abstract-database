dca85499b29ed1f24d408385d38271be093d1206
Frank Merema <frank@frontmen.nl>
2018-05-04 13:52:12 +0200

    Added generic database functionality

---
+++ src/database.ts
@@ -0,0 +1,82 @@
+import {
+    connect,
+    Mongoose,
+    model,
+    Model,
+    Schema,
+    Document,
+    QueryFindOneAndUpdateOptions,
+    ConnectionOptions
+} from 'mongoose';
+
+export class Database {
+
+    private readonly mongoose: Promise<Mongoose>;
+
+    constructor(host: string, port: number, database: string, options?: ConnectionOptions) {
+        this.mongoose = connect(`mongodb://${host}:${port}/${database}`, options);
+        this.mongoose.then(() => {
+            console.log('Connected to MongoDB');
+        }).catch(error => {
+            console.log(error);
+            throw error;
+        });
+    }
+
+    getConnection(): Promise<Mongoose> {
+        return this.mongoose;
+    }
+}
+
+
+export class Collection<T extends Document> {
+    private model: Model<T>;
+    private dbConnection: Promise<Mongoose>;
+
+    constructor(dbConnection: Promise<Mongoose>, name: string, schema: Schema, plural?: string) {
+        this.dbConnection = dbConnection;
+        this.model = model<T>(name, schema, plural || name);
+    }
+
+
+    save(data: T): Promise<T> {
+        return this.dbConnection
+            .then(() => {
+                return new this.model(data)
+                    .save();
+            });
+    }
+
+    find(mongoQuery: any, outputFields?: object, limit?: number): Promise<T[]> {
+        return this.dbConnection
+            .then(() => {
+                let query = this.model
+                    .find(mongoQuery, outputFields);
+
+                if (limit) {
+                    query = query.limit(limit);
+                }
+
+                return query.exec();
+            });
+    }
+
+    findOne(mongoQuery: any, outputFields?: object): Promise<T> {
+        return this.find(mongoQuery, outputFields)
+            .then((records: T[]) => records[0]);
+    }
+
+    findOneAndUpdate(mongoQuery: any, update: any, options?: QueryFindOneAndUpdateOptions): Promise<T> {
+        return this.dbConnection
+            .then(() => {
+                return this.model.findOneAndUpdate(mongoQuery, update, options);
+            });
+    }
+
+    findOneAndRemove(mongoQuery: any): Promise<T> {
+        return this.dbConnection
+            .then(() => {
+                return this.model.findOneAndRemove(mongoQuery);
+            });
+    }
+}
